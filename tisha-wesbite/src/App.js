6051407bdd7f95dfe288c5ceec32c50b825c77e6
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-09-07 16:56:35 -0500

    started the navbar

---
+++ tisha-wesbite/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import AppCoverpage from './AppCoverpage';
+
+function App() {
+  return (
+    <AppCoverpage/>
+  );
+}
+
+export default App;
---
+++ tisha-wesbite/src/AppCoverpage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import AuthNavBar from './Components/Navigation/AuthNavBar'
+
+export default function AppCoverpage() {
+  return (
+    <div><AuthNavBar/></div>
+  )
+}
