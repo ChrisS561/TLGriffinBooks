398e4e61efa50f2d643fbbb3959853f454e547c7
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2023-09-12 17:21:43 -0500

    Did the routing to different components. Not sure if we need to do them as Hashlinks though.

--- tisha-wesbite/src/Components/Navigation/NavBar.js
+++ tisha-wesbite/src/Components/Navigation/NavBar.js
@@ -1,146 +1,155 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import { common } from '@mui/material/colors';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import { common } from "@mui/material/colors";
 
-const pages = ['About', 'Books', 'Events', 'Contact'];
-const AuthSettings = ['Profile', 'Cart', 'Logout'];
-const Books = ['Book 1', 'Book 2', 'Book 3'];
+const pages = ["About", "Books", "Events", "Contact"];
+const AuthSettings = ["Profile", "Cart", "Logout"];
+const Books = ["Book 1", "Book 2", "Book 3"];
 
 function NavBar() {
-	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-	const handleOpenNavMenu = (event) => {
-		setAnchorElNav(event.currentTarget);
-	};
-	const handleOpenUserMenu = (event) => {
-		setAnchorElUser(event.currentTarget);
-	};
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
-	};
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-	return (
-		<AppBar position="static" sx={{ backgroundColor: '#FAF9F8' }}>
-			<Container maxWidth="xl">
-				<Toolbar>
-					{/* Bigger Screens */}
-					{/* Mobile devices */}
-					<Typography
-						variant="h5"
-						noWrap
-						component="a"
-						href="/"
-						sx={{
-							mr: 1,
-							display: { xs: 'none', md: 'flex' },
-							fontFamily: 'Playfair Display',
-							fontWeight: 700,
-							letterSpacing: '.3rem',
-							color: common.black,
-							textDecoration: 'none',
-						}}
-					>
-						T.L. Griffin
-					</Typography>
-					<Box
-						sx={{
-							flexGrow: 1,
-							display: { xs: 'flex', md: 'none' },
-						}}
-					>
-						<IconButton
-							size="large"
-							aria-label="account of current user"
-							aria-controls="menu-appbar"
-							aria-haspopup="true"
-							onClick={handleOpenNavMenu}
-							color="inherit"
-						>
-							<MenuIcon sx={{color: "black"}} />
-						</IconButton>
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#FAF9F8" }}>
+      <Container maxWidth="xl">
+        <Toolbar>
+          {/* Bigger Screens */}
+          {/* Mobile devices */}
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 1,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Playfair Display",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: common.black,
+              textDecoration: "none",
+            }}
+          >
+            T.L. Griffin
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon sx={{ color: "black" }} />
+            </IconButton>
 
-						<Menu
-							id="menu-appbar"
-							anchorEl={anchorElNav}
-							anchorOrigin={{
-								vertical: 'bottom',
-								horizontal: 'left',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'left',
-							}}
-							open={Boolean(anchorElNav)}
-							onClose={handleCloseNavMenu}
-							sx={{
-								display: { xs: 'block', md: 'none' },
-							}}
-						>
-							{pages.map((page) => (
-								<MenuItem key={page} onClick={handleCloseNavMenu}>
-									<Typography sx={{fontFamily: "inter"}} textAlign="center">{page}</Typography>
-								</MenuItem>
-							))}
-						</Menu>
-					</Box>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ fontFamily: "inter" }} textAlign="center">
+                    {page}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
 
-					<Typography
-						variant="h5"
-						noWrap
-						component="a"
-						href="/"
-						sx={{
-							mr: 2,
-							display: { xs: 'flex', md: 'none' },
-							flexGrow: 1,
-							fontFamily: 'Playfair Display',
-							fontWeight: 700,
-							letterSpacing: '.1px',
-							color: 'common.black',
-							textDecoration: 'none',
-						}}
-					>
-						T.L. Griffin
-					</Typography>
-					<Box
-						sx={{
-							flexGrow: 1,
-							display: {
-								xs: 'none',
-								md: 'flex',
-								justifyContent: 'space-evenly',
-							},
-						}}
-					>
-						{pages.map((page) => (
-							<Button
-								key={page}
-								onClick={handleCloseNavMenu}
-								sx={{ my: 2, color: 'black', display: 'block', fontSize: 16, fontFamily: "inter" }}
-							>
-								{page}
-							</Button>
-						))}
-					</Box>
-				</Toolbar>
-			</Container>
-		</AppBar>
-	);
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "Playfair Display",
+              fontWeight: 700,
+              letterSpacing: ".1px",
+              color: "common.black",
+              textDecoration: "none",
+            }}
+          >
+            T.L. Griffin
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: {
+                xs: "none",
+                md: "flex",
+                justifyContent: "space-evenly",
+              },
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "black",
+                  display: "block",
+                  fontSize: 16,
+                  fontFamily: "inter",
+                }}
+                href={`/${page}`}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 export default NavBar;
