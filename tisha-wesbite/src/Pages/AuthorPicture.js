542c1317822af4fd9039c3dc7a7638e06a967e27
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-09-11 22:15:04 -0500

    Added a new picture

--- tisha-wesbite/src/Pages/AuthorPicture.js
+++ tisha-wesbite/src/Pages/AuthorPicture.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import AuthorPic from "../Images/AuntTisha.jpg"
+import AuthorPic1 from "../Images/AuntTisha.png"
 import "../index.css"
 
 export default function AuthorPicture() {
   return (
 		<div className="">
 			<img
-				src={AuthorPic}
+				src={AuthorPic1}
 				alt="Profile"
 				className=" author-Picture "
         loading='lazy'
