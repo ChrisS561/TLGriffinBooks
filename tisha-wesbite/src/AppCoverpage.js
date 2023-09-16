fc6150b2817817f9209b3bd2d171ed8d77ae8074
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-09-16 01:29:14 -0500

    started the book display

--- tisha-wesbite/src/AppCoverpage.js
+++ tisha-wesbite/src/AppCoverpage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Homepage from './Pages/Homepage';
 import NavBar from './Components/Navigation/NavBar';
-
+import AuthorBookDisplay from './Components/AuthorBookDisplay';
 
 export default function AppCoverpage() {
 	return (
 		<div>
 			<NavBar />
 			<Homepage />
+			<AuthorBookDisplay/>
 		</div>
 	);
 }
---
+++ tisha-wesbite/src/Components/AuthorBookDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../index.css"
+
+export default function AuthorBookDisplay() {
+  return (
+    <div className='bookdisplay'>AuthorBookDisplay</div>
+  )
+}
