99a9e068b8e8a46bd3bf5c64e384ce24b711a850
SkyDe <sky_de1991@outlook.com>
2023-10-02 23:25:19 +0330

    new navbar done

--- tisha-wesbite/src/AppCoverpage.js
+++ tisha-wesbite/src/AppCoverpage.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Homepage from './Pages/Homepage';
-import NavBar from './Components/Navigation/NavBar';
-import AuthorBookDisplay from './Components/HomepageBookDisplay/AuthorBookSummary';
-import Footer from './Components/Footer/Footer';
+import React from "react";
+import Homepage from "./Pages/Homepage";
+import NavBar from "./Components/Navigation/NavBar";
+import AuthorBookDisplay from "./Components/HomepageBookDisplay/AuthorBookSummary";
+import Footer from "./Components/Footer/Footer";
 
 export default function AppCoverpage() {
-	return (
-		<div>
-			<NavBar />
-			<Homepage />
-			<AuthorBookDisplay />
-			<Footer/>
-		</div>
-	);
+  return (
+    <div style={{ maxWidth: "100vw", overflow: "hidden" }}>
+      <NavBar />
+      <Homepage />
+      <AuthorBookDisplay />
+      <Footer />
+    </div>
+  );
 }
