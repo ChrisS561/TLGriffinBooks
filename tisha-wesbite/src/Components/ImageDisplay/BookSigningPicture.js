2c945570592f783d8abb40ba7ded416ae03f44f9
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-12-01 12:25:23 -0600

    made the upcoming page more responsive

--- tisha-wesbite/src/Components/ImageDisplay/BookSigningPicture.js
+++ tisha-wesbite/src/Components/ImageDisplay/BookSigningPicture.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Booksigningpic from '../../Images/BookSigning.jpeg';
 import '../../index.css';
 import {  Box } from '@mui/material';
-import MobileBookSigningPicture from './MobileBookSigningPicture';
 
 export default function BookSigningPicture() {
 	return (
