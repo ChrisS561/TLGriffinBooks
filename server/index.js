8ebd2060008bd927036ce519a3b701dc0127a085
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-12-05 17:55:32 -0600

    update

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const sgMail = require('@sendgrid/mail');
 var admin = require('firebase-admin');
-"./"
+"/server/node_modules"
 dotenv.config();
 
 const app = express();
