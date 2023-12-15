4be544ade4f3f73887c518e6e78553f6c7351222
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-12-15 13:19:13 -0600

    made changes to the landing page background and made a alternative no upcoming events

--- tisha/src/Components/Homepage/EventsHomepage.js
+++ tisha/src/Components/Homepage/EventsHomepage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../../index.css';
 import { Box, Button, Stack, Typography } from '@mui/material';
+import { EventBusy as EventBusyIcon } from '@mui/icons-material'; // Import the EventBusy icon
 import HomepageEventsDate from './DateHomepage';
 import MobileHomepageEvents from './MobileEventsHomepage';
+import {  useRecoilValue } from 'recoil';
+import { noUpcomingEventAtom } from '../../atoms';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+
 
 export default function EventsHomepage() {
+	const noUpcomingEvents = useRecoilValue(noUpcomingEventAtom);
 	return (
 		<div className="events">
 			<Box>
@@ -46,63 +52,104 @@ export default function EventsHomepage() {
 							}}
 							gutterBottom
 						>
-							UPCOMING EVENTS
+							{noUpcomingEvents ? (
+								<>
+									No Upcoming Events{' '}
+									<EventBusyIcon
+										sx={{
+											fontSize: '2rem',
+											verticalAlign: 'middle',
+											marginLeft: '0.5rem',
+										}}
+									/>
+								</>
+							) : (
+								'Upcoming Events'
+							)}
 						</Typography>
-						<Typography
-							variant="body1"
-							sx={{
-								color: '#72320A',
-								textTransform: 'capitalize',
-								fontFamily: 'inter',
-								letterSpacing: '1px',
-								fontSize: { sm: 25, md: 30 },
-							}}
-							gutterBottom
-						>
-							A Womans Worth Book Signing
-						</Typography>
-						<Typography
-							variant="body1"
-							sx={{
-								color: '#72320A',
-								textTransform: 'capitalize',
-								textAlign: 'left',
-								fontFamily: 'inter',
-								letterSpacing: '1px',
-								fontSize: { sm: 25, md: 30 },
-							}}
-							gutterBottom
-						>
-							Austin, TX
-						</Typography>
+						{noUpcomingEvents && (
+							<>
+								<Typography
+									variant="body1"
+									sx={{
+										color: '#72320A',
+										textTransform: 'capitalize',
+										fontFamily: 'inter',
+										letterSpacing: '1px',
+										fontSize: { sm: 25, md: 30 },
+									}}
+									gutterBottom
+								>
+									Check back later for more events!
+								</Typography>
+								<SentimentVeryDissatisfiedIcon
+									sx={{
+										fontSize: 40,
+										color: '#72320A',
+										marginTop: '1rem',
+									}}
+								/>
+							</>
+						)}
+						{!noUpcomingEvents && (
+							<>
+								<Typography
+									variant="body1"
+									sx={{
+										color: '#72320A',
+										textTransform: 'capitalize',
+										fontFamily: 'inter',
+										letterSpacing: '1px',
+										fontSize: { sm: 25, md: 30 },
+									}}
+									gutterBottom
+								>
+									A Womans Worth Book Signing
+								</Typography>
+								<Typography
+									variant="body1"
+									sx={{
+										color: '#72320A',
+										textTransform: 'capitalize',
+										textAlign: 'left',
+										fontFamily: 'inter',
+										letterSpacing: '1px',
+										fontSize: { sm: 25, md: 30 },
+									}}
+									gutterBottom
+								>
+									Austin, TX
+								</Typography>
 
-						<Button
-							variant="contained"
-							href="/Upcoming-Events"
-							color="info"
-							sx={{
-								transition:
-									'box-shadow 0.3s ease, background-color 0.3s ease, color 0.3s ease',
-								':hover': {
-									boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.3)',
-									backgroundColor: '#FFBB57',
-									color: '#333',
-									animation: 'shake 0.5s',
-								},
-								display: 'block',
-								width: '100%',
-								maxWidth: '10rem',
-								fontFamily: 'inter',
-								fontSize: '15px',
-								letterSpacing: '2px',
-								textAlign: 'center',
-							}}
-						>
-							VIEW DETAILS
-						</Button>
+								<Button
+									variant="contained"
+									href="/Upcoming-Events"
+									color="info"
+									sx={{
+										transition:
+											'box-shadow 0.3s ease, background-color 0.3s ease, color 0.3s ease',
+										':hover': {
+											boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.3)',
+											backgroundColor: '#FFBB57',
+											color: '#333',
+											animation: 'shake 0.5s',
+										},
+										display: 'block',
+										width: '100%',
+										maxWidth: '10rem',
+										fontFamily: 'inter',
+										fontSize: '15px',
+										letterSpacing: '2px',
+										textAlign: 'center',
+									}}
+								>
+									VIEW DETAILS
+								</Button>
+							</>
+						)}
 					</Stack>
 				</Box>
-				<HomepageEventsDate />
+				{!noUpcomingEvents && <HomepageEventsDate />}
 			</Box>
 			<Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
 				{' '}
