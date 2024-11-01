68c13e3481fa673dc5b391088d28b918f0bf0873
chrissmith112 <chrissmithjr561@gmail.com>
2024-11-01 18:09:52 +0000

    Changed the writing

--- tisha/src/Pages/UpcomingEventsText.js
+++ tisha/src/Pages/UpcomingEventsText.js
@@ -5,23 +5,23 @@ import '../index.css';
 export default function UpcomingEventsText() {
 	const standoutText = 'Mark your calendar';
 	const text = `
-### More Details About the Book Signing
+More Details About the Book Signing
 
-**Event:** 14th Annual Women’s Health Motivation & Empowerment Conference
+Event: 14th Annual Women’s Health Motivation & Empowerment Conference
 
-**Location:** ACC Eastview Campus, 3401 Webberville Rd, Austin, TX
+Location: ACC Eastview Campus, 3401 Webberville Rd, Austin, TX
 
-**Date & Time:** November 2nd, 2024 from 8am - 4pm
+Date & Time: November 2nd, 2024 from 8am - 4pm
 
 Join me at the 14th Annual Women’s Health Motivation & Empowerment Conference, where I will be signing copies of my latest book! This empowering event celebrates women's health and wellness, featuring inspiring speakers, workshops, and community engagement.
 
-**What to Expect at the Book Signing:**
+What to Expect at the Book Signing:
 
-- **Meet & Greet:** I’d love to connect with you and hear your thoughts on the book!
-- **Personalized Signatures:** Get your book signed with a personal note.
-- **Discussion:** I’ll share insights into the book's journey and answer any questions you may have.
+- Meet & Greet: I’d love to connect with you and hear your thoughts on the book!
+- Personalized Signatures: Get your book signed with a personal note.
+- Discussion: I’ll share insights into the book's journey and answer any questions you may have.
 
-**Don’t miss out!** Bring your friends and family, and join us in celebrating women’s empowerment and health. I can’t wait to see you there!
+Don’t miss out! Bring your friends and family, and join us in celebrating women’s empowerment and health. I can’t wait to see you there!
 `;
 	return (
 		<Typography
